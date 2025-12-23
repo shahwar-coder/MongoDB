e9ca99f4240a70b93961b0031644589610ceb16b
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-12-23 08:53:03 +0530

    Create 07_help()_:_for_method?.py

---
+++ 07_help()_:_for_method?.py
@@ -0,0 +1,14 @@
+# No help() on method
+# Instead:
+# - write without parantheses
+
+# command:
+
+
+'''
+Q1. How do you get help for a specific method like `find()`?
+Ans. Type the method name without parentheses to see its usage.
+'''
+# Example
+# db.users.find
+# → shows how the function is defined and used
