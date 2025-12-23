e29e8fa451609a6167d27a33ca8ffd1ef56a9c01
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-12-23 15:24:52 +0530

    Add documentation for dropping 'managers' collection
    Document the process of dropping a MongoDB collection.

---
+++ 08_drop_collection2.py
@@ -0,0 +1,22 @@
+# > db
+# < office
+
+# > db.createCollection("managers")
+# < { ok: 1 }
+
+# > show collections
+# < employees
+#   managers
+
+# > db.managers.drop()
+# < true
+
+# > show collections
+# < employees
+
+# office>
+
+
+'''
+This is easy to follow, just simply observe the flow.
+'''
